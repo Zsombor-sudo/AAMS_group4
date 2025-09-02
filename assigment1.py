c99af8eabb0c391b57be3ca9271d2f53ba3dd6a4
albaarcosribas <albaa.blanes@gmail.com>
2025-09-02 13:18:56 +0200

    Update assigment1.py

--- assigment1.py
+++ assigment1.py
@@ -8,7 +8,7 @@ for i in range(300): # run the simulation for 300 steps
 
    # MOVE MULTIPLE ROBOTS
     actions = [np.array([1.0, 0.5]), np.array([0.5, 0.5]), np.array([0.2, 1.0]), np.array([1.5, 1.0]), np.array([0.7, 0.9])]
-    env.step(actions, action_id=[0, 1, 2, 3, 4, 5])  # Move robots 0 and 1
+    env.step(actions, action_id=[0, 1, 2, 3, 4, 5])  # Move robots 0 to 5
 
     #MOVE 1 ROBOT
     #env.step(np.array([0.5, 0.5]))  # 1.0 m/s forward, 0.5 rad/s turn
