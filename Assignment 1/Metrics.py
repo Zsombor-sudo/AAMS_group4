de09bea07ef32c3464220f488c72c79e0e1b174d
martinsekd <martinsekd@gmail.com>
2025-09-30 11:54:12 +0200

    Saving errors to a csv file

--- Assignment 1/Metrics.py
+++ Assignment 1/Metrics.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import csv
 
 class Metrics:
     numberOfAgents = 4
@@ -14,14 +15,25 @@ class Metrics:
         self.iterations = [1] * self.numberOfAgents
         self.circle_radius = circle_radius
         self.distance = [0] * self.numberOfAgents
+
+        #open the csv file to save measurements
+        self.f = open("Assignment 1/errors.csv", "a", newline="")
+        self.writer = csv.writer(self.f)
+        
     def setCircleRadius(self, radius):
         self.circle_radius = radius
     def update(self, robotid, distance):
         #printing error
         self.distance[robotid] = distance
         self.accDistance[robotid] += abs(distance - self.circle_radius)
-        self.iterations[robotid] += 1    
+        self.iterations[robotid] += 1
+        
+
     def print(self,robotid):
         #printing error
         self.errLabelText[robotid].set_text(f" {round(abs(self.distance[robotid] - self.circle_radius),2)}  ||  {round(self.accDistance[robotid], 2)}  ||  {round(self.accDistance[robotid]/self.iterations[robotid], 2)}")
+
+        #save to file
+        self.writer.writerow(round(abs(dist - self.circle_radius),2) for dist in self.distance)
+        self.f.flush()
 
