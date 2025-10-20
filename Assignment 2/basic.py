45c02452fe50317e0936782fd3963d5966071984
CoolEou <115168150+CoolEou@users.noreply.github.com>
2025-10-20 16:02:21 +0200

    Started assignment 2

---
+++ Assignment 2/basic.py
@@ -0,0 +1,23 @@
+import irsim
+
+for i in range(1):
+    # env = irsim.make(save_ani=False, full=False, display=False)
+    env = irsim.make(save_ani=False, full=False, display=True)
+    # env.load_behavior("custom_basic_circle")
+
+    for _i in range(1000):
+        env.step()
+
+        for rob in env.robot_list:
+            if rob.fov_detect_object(env.robot):
+                detected_robots = rob.get_fov_detected_objects()
+                for detRob in detected_robots:
+                    print(f'Agent {rob.id} detected agent {detRob.id}')
+
+        env.render(0.01)
+
+        if env.done():
+            break
+
+    env.end(1)
+    
