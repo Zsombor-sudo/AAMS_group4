221c07d5748c1369decf92a9f9e2d6eff855fabf
CoolEou <115168150+CoolEou@users.noreply.github.com>
2025-10-20 12:10:17 +0200

    Cleaned up the behaviours a bit

--- Assignment 1/basic.py
+++ Assignment 1/basic.py
@@ -1,25 +1,10 @@
 import irsim
 
-# Stuff seems to not work yet propperly yet, the agent seems to want to
-# go towards the goal rather than stay on the circle.
-# My theories is that either the agents turn radius is messing with it partly,
-# there are too many states, which gets too confusing for the agent or my reward
-# system still doesn't work properly
-# I could try to reduce the amount of actions down to two; going away from the goal
-# and going towards it.
-
-# I found out that i had forgotten to update the old_distance value, so i wasn't 
-# updating the Q table propperly
-
-# I still think i should reduce the complexity of the environment (amount of states and actions)
-
-# Reduced states down to 50 and actions down to 10, seems to work better now, still doesn't follow the circle that well tho
-# So maybe try and tweak the reward system more
-
 for i in range(1):
     # env = irsim.make(save_ani=False, full=False, display=False)
     env = irsim.make(save_ani=False, full=False, display=True)
-    env.load_behavior("custom_behavior_methods")
+    env.load_behavior("custom_basic_circle")
+    env.load_behavior("custom_RL_circle")
 
     for _i in range(500):
         env.step()
--- Assignment 1/custom_RL_circle.py
+++ Assignment 1/custom_RL_circle.py
@@ -5,87 +5,6 @@ from pathlib import Path
 import random
 import math
 
-@register_behavior("diff", "basic_circle")
-def beh_diff_dash(
-    ego_object: any, external_objects: list[any], **kwargs: any
-) -> np.ndarray:
-    """
-    Behavior function for differential drive robot using dash-to-goal behavior.
-
-    Args:
-        ego_object: The ego robot object.
-        external_objects (list): List of external objects in the environment.
-        **kwargs: Additional keyword arguments:
-            - angle_tolerance (float): Allowable angular deviation, default 0.1.
-
-    Returns:
-        np.array: Velocity [linear, angular] (2x1) for differential drive.
-    """
-
-    state = ego_object.state
-    goal = ego_object.goal
-    goal_threshold = ego_object.goal_threshold
-    _, max_vel = ego_object.get_vel_range()
-    angle_tolerance = kwargs.get("angle_tolerance", 0.1)
-    circle_radius = kwargs.get("circle_radius", 5)
-    correction_multiplier = kwargs.get("correction_multiplier", 1)
-
-    if goal is None:
-        if world_param.count % 10 == 0:
-            env_param.logger.warning(
-                "Goal is currently None. This dash behavior is waiting for goal configuration"
-            )
-
-        return np.zeros((2, 1))
-
-    return basic_circle(state, goal, max_vel, goal_threshold, angle_tolerance, circle_radius, correction_multiplier)
-
-
-def basic_circle(
-    state: np.ndarray,
-    goal: np.ndarray,
-    max_vel: np.ndarray,
-    goal_threshold: float = 0.1,
-    angle_tolerance: float = 0.2,
-    circle_radius: float = 5,
-    correction_multiplier: float = 1,
-) -> np.ndarray:
-    """
-    Calculate the differential drive velocity to reach a goal.
-
-    Args:
-        state (np.array): Current state [x, y, theta] (3x1).
-        goal (np.array): Goal position [x, y] (2x1).
-        max_vel (np.array): Maximum velocity [linear, angular] (2x1).
-        goal_threshold (float): Distance threshold to consider goal reached (default 0.1).
-        angle_tolerance (float): Allowable angular deviation (default 0.2).
-
-    Returns:
-        np.array: Velocity [linear, angular] (2x1).
-    """
-    distance, radian = relative_position(state, goal)
-
-    # if distance < goal_threshold:
-    #     return np.zeros((2, 1))
-
-    # Attempt at integral error corection
-    # distanceCorrection = 0
-    # distanceCorrection += (circle_radius - distance) * correction_multiplier
-    
-    distanceCorrection = (circle_radius - distance) * correction_multiplier
-    radian += np.pi/2 + distanceCorrection     # Attempt to change the desired angle to 90 degrees to the angle between the goal and the object
-
-    diff_radian = WrapToPi(radian - state[2, 0])
-    linear = max_vel[0, 0] * np.cos(diff_radian)
-
-    # if abs(diff_radian) < angle_tolerance:
-    #     angular = 0
-    # else:
-    #     angular = max_vel[1, 0] * np.sign(diff_radian)
-    angular = max_vel[1, 0] * np.sign(diff_radian)
-
-    return np.array([[linear], [angular]])
-
 @register_behavior("diff", "RL_circle")
 def beh_diff_dash(
     ego_object: any, external_objects: list[any], **kwargs: any
@@ -148,20 +67,10 @@ def RL_circle(
     
     distance, radian = relative_position(state, goal) # Get distance and angle to circle center
     distance = int(round(distance, 1) * 10) # Round the distance to nearest single decimal and multiply by 10 to get the current state
-    # distance = int(round(distance)) # Round the distance to nearest integer to get the current state
-    
-    # print(distance)
 
     # Calculate new Q value:
     if (RL_circle.old_values[agent_id, 0] != 0):
         ### Tip: Try to use https://www.geogebra.org/classic?lang=en to visualise the reward curve
-        # reward = -abs(circle_radius*10 - distance) * 100
-        # reward = -abs(circle_radius - distance) * 100
-        # reward = (math.pow(np.finfo(np.float32).eps, math.pow(-(distance - circle_radius), 2))) * 100  
-        # reward = 5 * (1 / (1 + 0.1 * math.pow(distance - circle_radius, 2))) - 0.5
-        # reward = 50 * (1 / (1 + 0.01 * math.pow(distance - circle_radius*10, 2))) - 5
-        # reward = -0.5 * math.pow(distance - circle_radius, 2)
-        # reward = -0.025 * math.pow(distance - circle_radius*10, 2)
         circle_radius = circle_radius * 10
         if distance > circle_radius:
             reward = 2*(-distance + circle_radius)
@@ -196,12 +105,9 @@ def RL_circle(
     # Calculate angular:
     radian += (action / RL_circle.q_table.shape[1]) * np.pi/2 + np.pi/4
     diff_radian = WrapToPi(radian - state[2, 0])
-    # angular = max_vel[1, 0] * np.sign(diff_radian)
-    # angular = np.clip(1 * diff_radian, -max_vel[1, 0], max_vel[1, 0])
     angular = max_vel[1, 0] * np.tanh(1 * diff_radian)
 
-    # Always move forward at a set speed
-    # linear = max_vel[0, 0] * np.cos(diff_radian)
+    # Always move forward
     linear = max_vel[0, 0] * (np.cos(diff_radian) + 1) / 2  
 
     return np.array([[linear], [angular]]) 
