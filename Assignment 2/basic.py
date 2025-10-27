648168f28144b362ebe256bdbc66e0d62f066448
CoolEou <115168150+CoolEou@users.noreply.github.com>
2025-10-27 14:14:50 +0100

    Got it somewhat working

--- Assignment 2/basic.py
+++ Assignment 2/basic.py
@@ -8,11 +8,11 @@ for i in range(1):
     for _i in range(1000):
         env.step()
 
-        for rob in env.robot_list:
-            if rob.fov_detect_object(env.robot):
-                detected_robots = rob.get_fov_detected_objects()
-                for detRob in detected_robots:
-                    print(f'Agent {rob.id} detected agent {detRob.id}')
+        # for rob in env.robot_list:
+        #     if rob.fov_detect_object(env.robot):
+        #         detected_robots = rob.get_fov_detected_objects()
+        #         for detRob in detected_robots:
+                    # print(f'Agent {rob.id} detected agent {detRob.id}')
 
         env.render(0.01)
 
--- Assignment 2/custom_follower_behaviour.py
+++ Assignment 2/custom_follower_behaviour.py
@@ -10,14 +10,17 @@ from irsim.util.util import WrapToPi, omni_to_diff, relative_position
 
 @register_behavior("diff", "follow_leader")
 def beh_diff_followLeader(
-    ego_object: any, external_objects: list[any], **kwargs: any
+    ego_object: Any, external_objects: list[Any], **kwargs: Any
 ) -> np.ndarray:
 
     state = ego_object.state
     _, max_vel = ego_object.get_vel_range()
     leader = external_objects[kwargs.get("leaderID", 0)]
+    angle_tolerance = kwargs.get("angle_tolerance", 0.1)
 
-    behavior_vel = followLeader(state, max_vel, leader)
+    neighbor_list = ego_object.get_fov_detected_objects()
+
+    behavior_vel = followLeader(state, max_vel, leader, angle_tolerance, neighbor_list)
 
     return behavior_vel
 
@@ -26,12 +29,25 @@ def followLeader(
     state: np.ndarray,
     max_vel: np.ndarray,
     leader: np.ndarray,
+    angle_tolerance: float = 0.2,
+    neighbor_list: Optional[list[Any]] = None,
 ) -> np.ndarray:
     distance, radian = relative_position(state, leader.state)
 
     diff_radian = WrapToPi(radian - state[2, 0])
-    linear = max_vel[0, 0] * np.cos(diff_radian)
+    linear = (max_vel[0, 0] * np.cos(diff_radian)) / 2
 
-    angular = max_vel[1, 0] * np.sign(diff_radian)
+    if abs(diff_radian) < angle_tolerance:
+        angular = 0
+    else:
+        angular = max_vel[1, 0] * np.sign(diff_radian)
+
+    if neighbor_list is not None:
+        for neighbor in neighbor_list:
+            distance, radian = relative_position(state, neighbor.state)
+            diff_radian = WrapToPi(radian - state[2, 0])
+
+            # linear += (max_vel[0, 0] * np.sin(diff_radian)) / (2 * len(neighbor_list))
+            linear += (max_vel[0, 0] * -np.cos(diff_radian)) / (2 * len(neighbor_list))
 
     return np.array([[linear], [angular]])
